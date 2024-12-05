0c78d4462080852153cab7ab682d11c6626b3091
Blue000Black <129663844+Blue000Black@users.noreply.github.com>
2024-12-05 18:20:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,35 @@ from random import randint
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.QtGui import QPainter, QColor
 import sys
-from PyQt6 import uic
+from PyQt6 import QtCore, QtWidgets
 
 
-class MainWin(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.btn = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.btn.setGeometry(QtCore.QRect(250, 500, 261, 51))
+        self.btn.setObjectName("btn")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(parent=MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.btn.setText(_translate("MainWindow", "Нарисовать круг"))
+
+
+class MainWin(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.setWindowTitle("Жёлтые круги")
         self.circle = [0, 0 ,0]
         self.btn.clicked.connect(self.act)
